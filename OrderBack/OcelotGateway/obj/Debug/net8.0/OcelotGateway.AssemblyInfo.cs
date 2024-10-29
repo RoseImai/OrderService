6f0d5d2cb40ad583390645f415876d07e7b7b399
RoseImai <herobrine2mikey@gmail.com>
2024-10-29 16:50:46 +0600

    Added Delivery in Ocelot

--- OrderBack/OcelotGateway/obj/Debug/net8.0/OcelotGateway.AssemblyInfo.cs
+++ OrderBack/OcelotGateway/obj/Debug/net8.0/OcelotGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OcelotGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35e22cdd6768c25d0aeedd2b9d913fc0b223ef97")]
 [assembly: System.Reflection.AssemblyProductAttribute("OcelotGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OcelotGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
